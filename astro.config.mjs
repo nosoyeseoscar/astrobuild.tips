b9977146749effd4c97efc4195e29f4771c70519
Oscar Fuentes <ofuentese@gmail.com>
2023-09-15 12:37:59 -0700

    [ADD] SEO, RSS, Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  site: 'https://localhost:4321',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
